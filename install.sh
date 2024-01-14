e31a13c847a34e91eebea3fbd52e3e08a73ed917
Sun Kit Tsui <sunkittsui@gmail.com>
2024-01-13 20:30:30 -0600

    First full edition

--- install.sh
+++ install.sh
@@ -1,11 +1,67 @@
 #!/bin/sh
 
-UPDATE_ARGS="-Syu --noconfirm"
-INSTALL_ARGS="-S --noconfirm"
+if [ "$SETUP_COMPLETE" != "yes" ]; then
+  echo "Please run setup.sh first before you run this script."
+  exit 1
+fi
 
-# Update system
-sudo pacman "$UPDATE_ARGS"
+CWD=$(pwd)
 
-# Install git
+# WARN: Don't double quote this variable for yay and pacman
+INSTALL_ARGS="-S --needed --noconfirm"
 
-packages="$(grep -v '#' packages | sed '/^$/d')"
+LOCAL_SRC="$HOME/.local/src"
+
+DOTFILES_DIR="$HOME/.dotfiles"
+DWM_DIR="$LOCAL_SRC/dwm"
+SLOCK_DIR="$LOCAL_SRC/slock"
+
+echo "Installing packages..."
+
+# Read packages
+PACKAGES="$(grep -v '#' packages | sed '/^$/d')"
+
+# Install packages with yay
+yay $INSTALL_ARGS $PACKAGES
+
+# Change default shell to zsh
+sudo chsh -s /usr/bin/zsh "$USER"
+
+# Install fonts
+sudo pacman $INSTALL_ARGS tty-fira-code tty-fira-mono tty-firacode-nerd wqy-zenhei
+
+# Pull configs from Github
+echo "Pulling dotfiles..."
+git clone git@github.com:sunkit02/dotfiles.git "$DOTFILES_DIR"
+
+echo "Pulling dwm..."
+git clone git@github.com:sunkit02/dwm.git "$DWM_DIR"
+
+echo "Pulling slock..."
+git clone git@github.com:sunkit02/slock.git "$SLOCK_DIR"
+
+# Configure dotfiles
+cd "$DOTFILES_DIR" || exit 1
+./install.sh
+
+# Build DWM
+cd "$DWM_DIR" || exit 1
+
+# TODO: Move installing this dependency to DWM itself
+sudo pacman $INSTALL_ARGS yajl
+
+sudo make clean install
+
+# Build slock
+cd "$SLOCK_DIR" || exit 1
+
+# Replace sunkit with the current user name in config.h in-place
+# TODO: Make this more elegant
+sed -i "s/sunkit/$USER/g" config.h
+
+# Only install for current user due to location of screensaver image
+make clean install
+
+# Closing instructions
+echo "Installation complete!"
+echo "Run 'startx' to start the X11 server and DWM."
---
+++ setup.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# WARN: Don't double quote these two variable for yay and pacman
+UPDATE_ARGS="-Syu --noconfirm"
+INSTALL_ARGS="-S --needed --noconfirm"
+
+LOCAL_SRC="$HOME/.local/src"
+
+# Update system
+sudo pacman $UPDATE_ARGS
+
+# Install git
+echo "Installing Git..."
+
+sudo pacman $INSTALL_ARGS base base-devel git
+
+if [ ! $? ]; then
+  printf "${RED}Failed to install Git.${NC}\n"
+  exit 1
+fi
+
+# Install yay
+echo "Installing Yay..."
+
+mkdir -p "$LOCAL_SRC"
+cd "$LOCAL_SRC" || exit 1
+
+git clone https://aur.archlinux.org/yay.git
+
+cd yay || exit 1
+makepkg -si --noconfirm
+yay --version || (printf "${RED}Failed to install Yay.${NC}\n" && exit 1)
+
+# Generate Github SSH key
+SSH_DIR="$HOME/.ssh"
+mkdir -p "$SSH_DIR"
+
+SSH_KEY="$SSH_DIR/github"
+printf '\n' | ssh-keygen -t ed25519 -f "$SSH_KEY"
+
+# Add SSH key to ssh-agent
+eval "$(ssh-agent -s)"
+ssh-add "$SSH_KEY"
+
+# Prompt user to add SSH key to Github
+printf "Add the following SSH key at %s to your Github. Checkout: https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account for help.\n" "$SSH_KEY.pub"
+echo "Copy the following:"
+cat "$SSH_KEY"
+
+export SETUP_COMPLETE=yes
