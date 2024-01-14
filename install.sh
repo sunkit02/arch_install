8aea47f1e63af470c7f3ddff84eb3f476cd7947e
Sun Kit Tsui <sunkittsui@gmail.com>
2024-01-14 15:12:05 -0600

    Add polybar-dwm-module to packages and move dwm deps to packages

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ DOTFILES_DIR="$HOME/.dotfiles"
 
 DWM_DIR="$LOCAL_SRC/dwm"
 SLOCK_DIR="$LOCAL_SRC/slock"
+SCRIPTS_DIR="$LOCAL_SRC/scripts"
 
 SSH_KEY="$HOME/.ssh/github"
 
@@ -45,6 +46,9 @@ eval "$(ssh-agent -s)" && ssh-add ~/.ssh/github
 echo "Pulling dotfiles..."
 git clone git@github.com:sunkit02/dotfiles.git "$DOTFILES_DIR"
 
+echo "Pulling scripts..."
+git clone git@github.com:sunkit02/my-local-shell-scripts.git "$SCRIPTS_DIR"
+
 echo "Pulling dwm..."
 git clone git@github.com:sunkit02/dwm.git "$DWM_DIR"
 
@@ -59,12 +63,12 @@ cd "$DOTFILES_DIR" || (echo "Failed to cd into $DOTFILES_DIR" && exit 1)
 cd "$DOTFILES_DIR/config/tmux" || (echo "Failed to cd into tmux" && exit 1)
 sh setup.sh
 
+# Deploy scripts
+cd "$SCRIPTS_DIR" || (echo "Failed to cd into scripts" && exit 1)
+./deploy_scripts.sh
+
 # Build DWM
 cd "$DWM_DIR" || (echo "Failed to cd into dwm" && exit 1)
-
-# TODO: Move installing these dependencies to DWM itself
-sudo pacman $INSTALL_ARGS yajl imlib2
-
 sudo make clean install
 
 # Build slock
